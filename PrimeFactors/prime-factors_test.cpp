27e6c2619078f08393b45f3864df039ad96ab1f0
yujung <unichoi92@hanmail.net>
2025-04-11 14:21:14 +0900

    of12 pass

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -39,6 +39,11 @@ TEST_F(PrimeFixture, Of9) {
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
 
+TEST_F(PrimeFixture, Of12) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
+
 int main()
 {
 	::testing::InitGoogleMock();
